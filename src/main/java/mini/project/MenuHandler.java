9ab309e11eaaac6277989cfae86ec76340489848
TaeheeKim15 <TaeheeKim15@github.com>
2020-09-09 19:13:03 +0900

    메뉴

---
+++ src/main/java/mini/project/MenuHandler.java
@@ -0,0 +1,35 @@
+package mini.project;
+
+import java.util.ArrayList;
+
+public class MenuHandler {
+
+	ArrayList<Menu> menuList;
+
+	public MenuHandler(ArrayList<Menu> ArrayList) {
+		this.menuList = ArrayList;
+	}
+
+	public void add() {
+		System.out.println("[메뉴 선정]");
+
+		Menu menu = new Menu();
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
