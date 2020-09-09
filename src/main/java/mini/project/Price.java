92f0d2855eb23455b68600f343b95e6ecc8a1f29
TaeheeKim15 <TaeheeKim15@github.com>
2020-09-09 16:40:24 +0900

    price 추가

---
+++ src/main/java/mini/project/Price.java
@@ -0,0 +1,36 @@
+package mini.project;
+
+public class Price {
+	private int dishname;
+	private int sidename;
+	private int beveragename;
+	private int alcoholname;
+	public int getDishname() {
+		return dishname;
+
+	}
+	public void setDishname(int dishname) {
+		this.dishname = dishname;
+	}
+	public int getSidename() {
+		return sidename;
+	}
+	public void setSidename(int sidename) {
+		this.sidename = sidename;
+	}
+	public int getBeveragename() {
+		return beveragename;
+	}
+	public void setBeveragename(int beveragename) {
+		this.beveragename = beveragename;
+	}
+	public int getAlcoholname() {
+		return alcoholname;
+	}
+	public void setAlcoholname(int alcoholname) {
+		this.alcoholname = alcoholname;
+	}
+
+
+}
+
