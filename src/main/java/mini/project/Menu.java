77a50bff2bc1ec4a80837ba98d4a9e315aa28f6e
TaeheeKim15 <TaeheeKim15@github.com>
2020-09-09 20:27:50 +0900

    수정

--- src/main/java/mini/project/Menu.java
+++ src/main/java/mini/project/Menu.java
@@ -1,36 +1,53 @@
 package mini.project;
 
 public class Menu {
-	private String dish;
-	private String side;
-	private String beverage;
-	private String alcohol;
+	private int dish;
+	private int side;
+	private int beverage;
+	private int alcohol;
+	private int amount;
+	private int menuNo;
 
 
-	public String getDish() {
+
+	public int getMenuNo() {
+		return menuNo;
+	}
+	public void setMenuNo(int menuNo) {
+		this.menuNo = menuNo;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public int getDish() {
 		return dish;
 	}
-	public void setDish(String dish) {
+	public void setDish(int dish) {
 		this.dish = dish;
 	}
-	public String getSide() {
+	public int getSide() {
 		return side;
 	}
-	public void setSide(String side) {
+	public void setSide(int side) {
 		this.side = side;
 	}
-	public String getBeverage() {
+	public int getBeverage() {
 		return beverage;
 	}
-	public void setBeverage(String beverage) {
+	public void setBeverage(int beverage) {
 		this.beverage = beverage;
 	}
-	public String getAlcohol() {
+	public int getAlcohol() {
 		return alcohol;
 	}
-	public void setAlcohol(String alcohol) {
+	public void setAlcohol(int alcohol) {
 		this.alcohol = alcohol;
 	}
+
+
 
 
 }
--- src/main/java/mini/project/MenuHandler.java
+++ src/main/java/mini/project/MenuHandler.java
@@ -17,10 +17,69 @@ public class MenuHandler {
 
 		Menu menu = new Menu();
 
-		menu.setDish(Prompt.inputInt("메인 음식 번호를 입력해주세요 : "));
-		menu.setSide(Prompt.inputInt("사이드 음식 번호를 입력해주세요 : "));
-		menu.setBeverage(Prompt.inputInt("음료 번호를 입력해주세요: "));
+		menu.setDish(Prompt.inputInt("메인 음식 번호를 입력해주세요\n"
+				+ "0: 바지락 칼국수\n"
+				+ "1: 팥 칼국수\n"
+				+ "2: 들깨 칼국수\n"
+				+ "3: 새알팥죽"));
+		String food = null;
+		switch(menu.getDish()) {
+		case 1:
+			food = "바지락 칼국수";
+			break;
+		case 2:
+			food = "팥 칼국수";
+			break;
+		case 3:
+			food = "들깨 칼국수";
+
+
+		}
+		menu.setSide(Prompt.inputInt("사이드 음식 번호를 입력해주세요\n"
+				+ "5: 해물파전\n"
+				+ "6: 왕만두"));
+		menu.setBeverage(Prompt.inputInt("음료 번호를 입력해주세요\n"
+				+ "7: 콜라\n"
+				+ "8: 사이다"));
+		menu.setAlcohol(Prompt.inputInt("주류 번호를 입력해주세요\n"
+				+ "9: 소주\n"
+				+ "10: 맥주\n"
+				+ "11: 막걸리"));
+
+		menuList.add(menu);
 	}
+
+	public void list() {
+		System.out.println("[주문 조회]");
+
+		for (int i = 0; i < menuList.size(); i++) {
+			Menu menu = menuList.get(i);
+			System.out.printf("%d, %d, %d, %d\n",
+					menu.getDish(),
+					menu.getSide(),
+					menu.getBeverage(),
+					menu.getAlcohol());
+		}
+	}
+
+
+	public void change() {
+		System.out.println("[메뉴 변경]");
+		int no = Prompt.inputInt("변경하실 메뉴의 번호를 입력해주세요.");
+		Menu menu = 
+	}
+
+	private Menu findByNo(int no) {
+		for (int i = 0; i < menuList.size(); i++) {
+			Menu menu = menuList.get(i);
+			if (menu.getNo() == no) {
+				return menu;
+			}
+		}
+		return null;
+	}
+
+
 }
 
 
